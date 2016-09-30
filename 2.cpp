da8b487d05de3b5aaa9959d0c1c76f005852311d
bektemirova17 <arok_98@mail.ru>
2016-10-01 05:37:49 +0600

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int(){
+cout<<" *********         ***             *             *";
+cout<<" *       *       *     *          ***           * * ";
+cout<<" *       *      *       *        *****         *   *";
+cout<<" *       *      *       *          *          *     * ";
+cout<<" *       *      *       *          *         *       *  ";
+cout<<" *       *      *       *          *          *     * ";
+cout<<" *       *      *       *          *           *   * ";
+cout<<" *       *       *     *           *            * *  ";
+cout<<" *********         ***             *             *  ";
+return 0;
+}
---
+++ 4.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {                                                
+	cout<<"Bektemirova" <<"\t" << "Aruzhan" << endl;
+	cout<<"Orazgalieva" <<"\t" << "Assem"   <<endl;
+	cout<<"Zarkymova" <<"\t" <<"Zere";
+	return 0;
+}
